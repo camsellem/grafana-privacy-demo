c2624296e9297417e03974490564e1ee019f261d
Cyril Amsellem <camsellem@gmail.com>
2022-11-08 14:35:37 +1100

    Add .gitignore

--- demo.sh
+++ demo.sh
@@ -14,15 +14,15 @@
 # GC_API_KEY          - Your Grafana Cloud API Key
 ################################################################
 
-DOCKER_IMG_REPO_URL=""
-K8S_AGENT_NAMESPACE=""
-GC_METRICS_ENDPOINT=""
-GC_METRICS_USERNAME=""
-GC_LOGS_ENDPOINT=""
-GC_LOGS_USERNAME=""
-GC_TRACES_ENDPOINT=""
-GC_TRACES_USERNAME=""
-GC_API_KEY=""
+DOCKER_IMG_REPO_URL="australia-southeast1-docker.pkg.dev/solutions-engineering-248511/camsellem-artifact-repo"
+K8S_AGENT_NAMESPACE="default"
+GC_METRICS_ENDPOINT="https://prometheus-prod-10-prod-us-central-0.grafana.net/api/prom/push"
+GC_METRICS_USERNAME="208413"
+GC_LOGS_ENDPOINT="https://logs-prod-us-central1.grafana.net/loki/api/v1/push"
+GC_LOGS_USERNAME="103178"
+GC_TRACES_ENDPOINT="https://prometheus-prod-10-prod-us-central-0.grafana.net/api/prom/push"
+GC_TRACES_USERNAME="99690"
+GC_API_KEY="eyJrIjoiNDUzZDk5ODVmYTQ0MGRmZGI2ZjQwNDljMDRhOGIxMjU5NzYzMDE1NiIsIm4iOiJjYW1zZWxsZW0tZWFzeXN0YXJ0LXByb20tcHVibGlzaGVyIiwiaWQiOjQ2ODg4MX0="
 
 clean() {
   SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
